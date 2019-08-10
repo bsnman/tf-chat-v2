a26124eaa809e9ffc91100b03ba4345f2171578e
bsnman <radihjrprgs@gmail.com>
2019-08-10 13:10:50 +0800

    initial commit startup quasar

---
+++ src/components/LayoutComponents/BaseHeader.vue
@@ -0,0 +1,30 @@
+<template>
+	<q-header elevated>
+		<q-toolbar>
+			<q-btn
+				flat
+				dense
+				round
+				@click="leftDrawerOpen = !leftDrawerOpen"
+				aria-label="Menu"
+			>
+				<q-icon name="menu" />
+			</q-btn>
+
+			<q-toolbar-title>
+				Quasar App
+			</q-toolbar-title>
+
+			<div>Quasar v{{ $q.version }}</div>
+		</q-toolbar>
+	</q-header>
+</template>
+
+<script>
+export default {
+	name: 'base-header',
+	methods: {
+	}
+}
+</script>
+
