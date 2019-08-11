a73badeecf6f1f7cb964333ea9fa58487fe8ae8e
bsnman <radihjrprgs@gmail.com>
2019-08-11 16:04:04 +0800

    remove vue-apollo, only use apolloClient

--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -1,35 +1,53 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import VueApollo from "vue-apollo";
-import fetch from "node-fetch";
 import { createHttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { onError } from "apollo-link-error";
+import { ApolloLink, split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import fetch from "node-fetch";
 
 const httpLink = createHttpLink({ uri: `${process.env.GRAPHQL_API}`, fetch });
 
-// Create the apollo client
-export const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-  connectToDevTools: true
+const wsLink = new WebSocketLink({
+  uri: `${process.env.GARPHQL_WEBSOCKET}`,
+  options: {
+    reconnect: true
+  }
 });
 
-export const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-  errorHandler({ graphQLErrors, networkError }) {
-    if (graphQLErrors) {
+const httpWsLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const link = ApolloLink.from([
+  onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
       graphQLErrors.map(({ message, locations, path }) =>
         console.log(
           `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
         )
       );
-    }
-    if (networkError) {
-      console.log(`[Network error]: ${networkError}`);
-    }
-  }
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  }),
+  httpWsLink
+]);
+
+// Create the apollo client
+export const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
 });
 
 export default ({ app, Vue }) => {
-  Vue.use(VueApollo);
-  app.apolloProvider = apolloProvider;
+  app.apolloClient = apolloClient;
 };
