3eff3014789e4b14e76040cfeb38428797934d31
bsnman <radihjrprgs@gmail.com>
2019-08-17 14:09:03 +0800

    updates

--- config/envparser.js
+++ config/envparser.js
@@ -2,9 +2,6 @@ const DotEnv = require("dotenv-flow");
 
 const parsedEnv = DotEnv.config().parsed;
 
-console.log("ENVIRONMENT == ", parsedEnv);
-console.log("ENVIRONMENT == ", process.env);
-
 module.exports = function() {
   // Let's stringify our variables
   for (const key in parsedEnv) {
@@ -12,7 +9,5 @@ module.exports = function() {
       parsedEnv[key] = JSON.stringify(parsedEnv[key]);
     }
   }
-
-  console.log("ENVIRONMENT == ", parsedEnv);
   return parsedEnv;
 };
