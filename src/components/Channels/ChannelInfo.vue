d76d8d69a36ced8dfa9ad16ee4ac237481ca526a
bsnman <radihjrprgs@gmail.com>
2019-08-17 14:51:21 +0800

    channel info and id

---
+++ src/components/Channels/ChannelInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="channel-info">
+    <q-list class="user-list">
+    <q-item-label header>Channel</q-item-label>
+      
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="fas fa-hashtag" />
+        </q-item-section>
+        <q-item-section>
+          <div>
+            <q-item-label>
+              {{ channel.title }}
+            </q-item-label>
+            <small>
+              <a @click="copyToClipboard(channel.id)">
+                {{ channel.id }}
+              </a>
+            </small>
+          </div>
+        </q-item-section>
+      </q-item>
+
+  </q-list>
+  </div>
+</template>
+
+<script>
+import { Notify } from 'quasar'
+export default {
+  name: "channel-info",
+  props: {
+    channel: {
+      type: Object, 
+      required: true
+    }
+  },
+  methods: {
+    copyToClipboard(id) {
+      Notify.create({
+        message: 'Copied to clipboard'
+      });
+    }
+  }
+}
+</script>
